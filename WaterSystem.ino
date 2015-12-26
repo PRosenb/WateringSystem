845964c6a4d4d26c2c2acdb0ecb526f6801d8c1d
peter.rosenberg <peter.rosenberg@bigtincan.com>
2015-12-27 00:15:49 +1100

    move stopAll to normal interrupt because it does not need to wake up (relevant on Arduino Uno)

--- WaterSystem.ino
+++ WaterSystem.ino
@@ -4,14 +4,20 @@
 #include "SleepManager.h"
 #include "WaterManager.h"
 
+#define STOP_ALL_INT_PIN 0
+
 SleepManager *sleepManager;
 WaterManager *waterManager;
+volatile boolean stopAllTriggered = false;
 
 void setup() {
   Serial.begin(9600);
   delay(100); // wait for serial to init
   sleepManager = new SleepManager();
   waterManager = new WaterManager();
+
+  pinMode(STOP_ALL_INT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(STOP_ALL_INT_PIN), isrStopAll, FALLING);
 
   //  RTC.setAlarm(ALM1_MATCH_SECONDS, 0, 0, 0, 1);
   //  RTC.alarmInterrupt(ALARM_1, true);
@@ -46,14 +52,18 @@ void loop() {
   }
   if (interrupts.button1) {
     Serial.println("BUTTON_1_INT");
-    waterManager->stopAll();
+    waterManager->manualMainOn();
   }
   if (interrupts.button2) {
     Serial.println("BUTTON_2_INT");
-    waterManager->manualMainOn();
+    waterManager->startAutomatic();
   }
   if (interrupts.button3) {
     Serial.println("BUTTON_3_INT");
+  }
+  if (stopAllTriggered) {
+    stopAllTriggered = false;
+    waterManager->stopAll();    
   }
 
   // allows also to sync time after wakeup
@@ -67,5 +77,9 @@ void rtcTriggered() {
   Serial.print("temp:");
   Serial.println(celsius, DEC);
   waterManager->startAutomaticWithWarn();
+}
+
+void isrStopAll() {
+  stopAllTriggered = true;
 }
 
