dcd2a25fda21ab9785cb6b4af869aff16ef8bc6b
Peter Rosenberg <p.rosenberg@gmx.net>
2016-07-05 20:52:33 +1000

    change ColorLedState to switch off led after configurable time

--- LedState.h
+++ LedState.h
@@ -4,24 +4,24 @@
 
 #include "DurationFsm.h"
 
-class ColorLedState: public DurationState {
+class ColorLedState: public DurationState, public Runnable {
   public:
     // the pins to set to LOW on enter, 255 for unused colors
-    ColorLedState(byte greenPin, byte redPin, byte bluePin, unsigned long durationMs, String name): DurationState(durationMs, name), greenPin(greenPin), redPin(redPin), bluePin(bluePin) {
+    ColorLedState(byte greenPin, byte redPin, byte bluePin, unsigned long ledOnDurationMs, String name)
+      : DurationState(INFINITE_DURATION, name), greenPin(greenPin), redPin(redPin), bluePin(bluePin), ledOnDurationMs(ledOnDurationMs) {
+    }
+    ColorLedState(byte greenPin, byte redPin, byte bluePin, unsigned long ledOnDurationMs, String name, SuperState * const superState)
+      : DurationState(INFINITE_DURATION, name, superState), greenPin(greenPin), redPin(redPin), bluePin(bluePin), ledOnDurationMs(ledOnDurationMs) {
     }
     virtual void enter() {
       reactivateLed();
+      if (ledOnDurationMs != INFINITE_DURATION) {
+        scheduler.scheduleDelayed(this, ledOnDurationMs);
+      }
     }
     virtual void exit() {
-      if (greenPin != 255) {
-        digitalWrite(greenPin, HIGH);
-      }
-      if (redPin != 255) {
-        digitalWrite(redPin, HIGH);
-      }
-      if (bluePin != 255) {
-        digitalWrite(bluePin, HIGH);
-      }
+      deactivateLed();
+      scheduler.removeCallbacks(this);
     }
     void reactivateLed() {
       if (greenPin != 255) {
@@ -34,8 +34,23 @@ class ColorLedState: public DurationState {
         digitalWrite(bluePin, LOW);
       }
     }
+    void deactivateLed() {
+      if (greenPin != 255) {
+        digitalWrite(greenPin, HIGH);
+      }
+      if (redPin != 255) {
+        digitalWrite(redPin, HIGH);
+      }
+      if (bluePin != 255) {
+        digitalWrite(bluePin, HIGH);
+      }
+    }
+    void run() {
+      deactivateLed();
+    }
   private:
     const byte greenPin, redPin, bluePin;
+    const unsigned long ledOnDurationMs;
 };
 
 #endif
