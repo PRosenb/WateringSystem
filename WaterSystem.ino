8078be45471db06c4831e316e7481e530e0ea9f4
PRosenb <p.rosenberg@gmx.net>
2017-09-08 22:01:10 +1000

    move superviseCrashResetCount() and initRtc() to separate methods

--- WaterSystem.ino
+++ WaterSystem.ino
@@ -18,46 +18,24 @@
 SerialManager *serialManager;
 WaterManager *waterManager;
 
-class SupervisionCallback: public Runnable {
-    void run() {
-      int resetCount = 0;
-      EEPROMwl.get(EEPROM_INDEX_WATCHDOG_RESET_COUNT, resetCount);
-      EEPROMwl.put(EEPROM_INDEX_WATCHDOG_RESET_COUNT, resetCount + 1);
-    }
-};
-
 void setup() {
-  EEPROMwl.begin(EEPROM_VERSION, EEPROM_INDEX_COUNT, EEPROM_LENGTH_TO_USE);
-  int resetCount = 0;
-  EEPROMwl.get(EEPROM_INDEX_WATCHDOG_RESET_COUNT, resetCount);
-  if (resetCount > 100) {
-    pinMode(MODE_COLOR_GREEN_PIN, OUTPUT);
-    digitalWrite(MODE_COLOR_GREEN_PIN, LOW);
-    pinMode(MODE_COLOR_RED_PIN, OUTPUT);
-    digitalWrite(MODE_COLOR_RED_PIN, LOW);
+  if (!superviseCrashResetCount()) {
+    // too many resets
     return;
   }
+
   serialManager = new SerialManager(BLUETOOTH_ENABLE_PIN);
   waterManager = new WaterManager();
   serialManager->setWaterManager(waterManager);
 
-  RTC.alarmInterrupt(ALARM_1, true);
-  RTC.alarmInterrupt(ALARM_2, false);
-  // reset alarms if active
-  RTC.alarm(ALARM_1);
-  RTC.alarm(ALARM_2);
-  delay(1000);
+  initRtc();
 
-  pinMode(RTC_INT_PIN, INPUT_PULLUP);
-  enableInterrupt(RTC_INT_PIN, isrRtc, FALLING);
   //  pinMode(START_MANUAL_PIN, INPUT_PULLUP);
   //  enableInterrupt(START_MANUAL_PIN, isrStartManual, FALLING);
   pinMode(START_AUTOMATIC_PIN, INPUT_PULLUP);
   enableInterrupt(START_AUTOMATIC_PIN, isrStartAutomatic, FALLING);
   pinMode(MODE_PIN, INPUT_PULLUP);
   enableInterrupt(MODE_PIN, isrMode, FALLING);
-
-  scheduler.setSupervisionCallback(new SupervisionCallback());
 
   serialManager->startSerial(SERIAL_SLEEP_TIMEOUT_MS);
 }
@@ -67,7 +45,60 @@ void loop() {
 }
 
 // ----------------------------------------------------------------------------------
-// interrupts
+// reset count supervision
+// ----------------------------------------------------------------------------------
+class SupervisionCallback: public Runnable {
+    void run() {
+      int resetCount = 0;
+      EEPROMwl.get(EEPROM_INDEX_WATCHDOG_RESET_COUNT, resetCount);
+      EEPROMwl.put(EEPROM_INDEX_WATCHDOG_RESET_COUNT, resetCount + 1);
+    }
+};
+
+/**
+   return false if not too many crash resets, true if system should stop.
+*/
+inline bool superviseCrashResetCount() {
+  EEPROMwl.begin(EEPROM_VERSION, EEPROM_INDEX_COUNT, EEPROM_LENGTH_TO_USE);
+  int resetCount = 0;
+  EEPROMwl.get(EEPROM_INDEX_WATCHDOG_RESET_COUNT, resetCount);
+  if (resetCount > 100) {
+    // too many crashes, prevent execution
+    pinMode(MODE_COLOR_GREEN_PIN, OUTPUT);
+    digitalWrite(MODE_COLOR_GREEN_PIN, LOW);
+    pinMode(MODE_COLOR_RED_PIN, OUTPUT);
+    digitalWrite(MODE_COLOR_RED_PIN, LOW);
+
+    Serial.begin(9600);
+    // wait for serial port to connect
+    while (!Serial);
+    Serial.println("too many resets");
+
+    return false;
+  } else {
+    // no problem, execute as normal
+    scheduler.setSupervisionCallback(new SupervisionCallback());
+    return true;
+  }
+}
+
+// ----------------------------------------------------------------------------------
+// RTC initialisation
+// ----------------------------------------------------------------------------------
+inline void initRtc() {
+  RTC.alarmInterrupt(ALARM_1, true);
+  RTC.alarmInterrupt(ALARM_2, false);
+  // reset alarms if active
+  RTC.alarm(ALARM_1);
+  RTC.alarm(ALARM_2);
+  delay(1000);
+  
+  pinMode(RTC_INT_PIN, INPUT_PULLUP);
+  enableInterrupt(RTC_INT_PIN, isrRtc, FALLING);
+}
+
+// ----------------------------------------------------------------------------------
+// interrupts and their callback methods
 // ----------------------------------------------------------------------------------
 void modeScheduled() {
   waterManager->modeClicked();
